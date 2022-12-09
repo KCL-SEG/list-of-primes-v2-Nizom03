2369296b7700ac77f7fcecb41219e3f63c3d40fb
Nizom03 <Nizom.badal@kcl.ac.uk>
2022-12-09 04:01:01 +0000

    done

--- primes.py
+++ primes.py
@@ -1,6 +1,16 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
-
+import math
 def primes(number_of_primes):
+    if (number_of_primes==0 ):
+        raise ValueError('A very specific bad thing happened.')
+    if (number_of_primes<0 ):
+        raise ValueError('A very specific bad thing happened.')
     list = []
+
+    for num in range(2,number_of_primes):
+            if (num%i!=0 for i in range(2,int(math.sqrt(num))+1)):
+                list.append(num)
+                # print(num)
+
     return list
